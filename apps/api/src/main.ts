8f4e58f66656a658c86b65670bd77c6a330f297e
young <young@overdare.com>
2025-11-14 13:57:01 +0900

    chore: changed port number

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3011',
     credentials: true,
   })
 
@@ -17,7 +17,7 @@ async function bootstrap() {
     }),
   )
 
-  const port = process.env.PORT || 3001
+  const port = process.env.PORT || 3010
   await app.listen(port)
   console.log(`Application is running on: http://localhost:${port}`)
 }
